8d4679334da48fa3536ea20c64ba0c68cebca9ee
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-06-23 17:40:28 +0200

    commit

--- homework.js
+++ homework.js
@@ -1,11 +1,49 @@
-const searchImages = (query) => {
-    fetch(`http://www.splashbase.co/api/v1/images/search?query=${query}`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        renderData(data.images);
-      })
-      .catch((err) => console.log(err));
-  };
+const renderData = (data) => {
+  const parent = document.querySelector(".album .row");
+  parent.innerHTML = "";
+  data.forEach((img) => {
+    const card = `   <div class="col-md-4">
+         <div class="card mb-4 shadow-sm">
+          <img src=""/>
+           <div class="card-body">
 
-  searchImages("cars")
+             <div
+               class="d-flex justify-content-between align-items-center"
+             >
+               <div class="btn-group">
+                 <button
+                   type="button"
+                   class="btn btn-sm btn-outline-secondary"
+                  onclick="showModal(event)"
+                  data-toggle="modal" data-target="#exampleModal"
+                 >
+                   View
+                 </button>
+                 <button
+                   type="button"
+                   class="btn btn-sm btn-outline-secondary"
+                   onclick="removeCard(event)"
+                 >
+                   Hide
+                 </button>
+               </div>
+               <small class="text-muted"></small>
+             </div>
+           </div>
+         </div>
+       </div>`;
+    parent.innerHTML += card;
+  });
+};
+
+const searchImages = (query) => {
+  fetch(`http://www.splashbase.co/api/v1/images/search?query=${query}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      renderData(data.images);
+    })
+    .catch((err) => console.log(err));
+};
+
+searchImages("cars");
